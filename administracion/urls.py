851d0cb86d740e49e3374ad195d1d38e3d8f3ac9
Eze0507 <xxezequielxx315@gmail.com>
2025-09-20 08:45:31 -0400

    implementacion del login

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.views import (
+    tokenObtainPairView,
+    tokenRefreshView,
+)
 
 urlpatterns = [
-
+    path('login/', tokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', tokenRefreshView.as_view(), name='token_refresh'),
 ]
--- condominioBACK/settings.py
+++ condominioBACK/settings.py
@@ -129,3 +129,13 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # o IsAuthenticated si quieres proteger todo
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
