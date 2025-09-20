253e431fabcb5bd65599823e48c7902add169587
Eze0507 <xxezequielxx315@gmail.com>
2025-09-20 13:51:31 -0400

    implementacion del gestionar usuario

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import LogoutView
+from .views import LogoutView, UserViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
--- administracion/views.py
+++ administracion/views.py
@@ -1,4 +1,7 @@
-from rest_framework import status
+from rest_framework import status, viewsets
+from django.contrib.auth.models import User
+from .serializers.serializersUser import UserSerializer
+from django.db.models import ProtectedError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -17,3 +20,18 @@ class LogoutView(APIView):
             return Response({"message": "Logout exitoso"}, status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        try:
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except ProtectedError:
+            return Response(
+                {"detail": "No se puede eliminar este usuario porque está asociado a otros registros (como un cliente o empleado)."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
