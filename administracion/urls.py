d13903728361e72a89aa136d745abd0c894df16e
Eze0507 <xxezequielxx315@gmail.com>
2025-09-20 10:56:11 -0400

    login update

--- administracion/urls.py
+++ administracion/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.views import (
-    tokenObtainPairView,
-    tokenRefreshView,
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
 )
 
 urlpatterns = [
-    path('login/', tokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', tokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
